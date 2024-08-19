cc1799ad5cd4eda78c9fa26299c4581a33b44431
MinGyu Jeong <tomorrow9913@gmail.com>
2024-08-19 16:14:02 +0900

    Feat: Send Data to Back

--- src/main/websrc/src/hooks/useSecureKeypad.jsx
+++ src/main/websrc/src/hooks/useSecureKeypad.jsx
@@ -4,6 +4,19 @@ import {useMemo, useState} from 'react';
 import axios from "axios";
 import {JSEncrypt} from "jsencrypt";
 
+function encryptMessage(publicKey, message) {
+  const encryptor = new JSEncrypt();
+  encryptor.setPublicKey(String(publicKey));
+  
+  const encryptedMessage = encryptor.encrypt(message);
+  
+  if (!encryptedMessage) {
+    throw new Error("Encryption failed.");
+  }
+  
+  return encryptedMessage;
+}
+
 export default function useSecureKeypad() {
   const [keypad, setKeypad] = useState(null);
   const [userInput, setUserInput] = useState([]);
@@ -11,6 +24,7 @@ export default function useSecureKeypad() {
   const getSecureKeypad = () => {
       axios.get('http://localhost:8080/api/create')
       .then(response => {
+        console.info(response.data);
         setKeypad(() => response.data);
       });
   }
@@ -24,8 +38,24 @@ export default function useSecureKeypad() {
   }
 
   const sendUserInput = (input_list) => {
-    alert(JSON.stringify(input_list));
-    location.reload();
+    let data = input_list.join(""); 
+    console.log("user input: " + data);
+
+    axios.post('http://localhost:8080/api/submit', {
+      keypadId: keypad.keypadId,
+      timeStamp: keypad.timeStamp,
+      input: encryptMessage(keypad.pubKey, data),
+      hash: keypad.hash,
+    })
+    .then(response => {
+      alert(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+      alert("Error submitting");
+    })
+    
+    setTimeout(() => location.reload(), 500);
   }
 
   return {
